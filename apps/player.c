16b2cf9c333920d0e01dc0bb71488e72a428cd04
Minhyun Kim <minionkim@outlook.com>
2015-09-30 02:03:22 +0900

    modification for hello branch

--- apps/player.c
+++ apps/player.c
@@ -3,10 +3,12 @@
 //#include "func2.h"
 
 // added into git repository
-
+// 2. src modified for hello branch
 int main(int argc, char* argv[])
 {
+	printf("hello branch");
 	foo();
+
 	bar();
 	return 0;
 }
