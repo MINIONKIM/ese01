900b6acfa470eb8369b692b6758ed94c84dc0fe3
Minhyun Kim <minionkim@outlook.com>
2015-09-30 02:04:20 +0900

    modification for master branch

--- apps/player.c
+++ apps/player.c
@@ -2,10 +2,11 @@
 //#include "func1.h"
 //#include "func2.h"
 
-// added into git repository
+// added into git repository + master modification
 
 int main(int argc, char* argv[])
 {
+	printf("master branch");
 	foo();
 	bar();
 	return 0;
