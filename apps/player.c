f2813f65feabe2b88cf60fa5c515345d4da8656b
Kim, Minhyun <minionkim@outlook.com>
2015-09-30 16:07:02 +0900

    rollback

--- apps/player.c
+++ apps/player.c
@@ -2,21 +2,9 @@
 //#include "func1.h"
 //#include "func2.h"
 
-<<<<<<< HEAD
-// added into git repository + master modification
-
-//int main(int argc, char* argv[])
-//{
-//	printf("master branch");
-=======
-// added into git repository
-// 2. src modified for hello branch
 int main(int argc, char* argv[])
 {
-	printf("hello branch");
->>>>>>> world
 	foo();
-
 	bar();
 	return 0;
 }
